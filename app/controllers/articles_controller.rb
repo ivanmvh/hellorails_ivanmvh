6537e1a3ce08394413fb412a00fe9f59b6f5ef11
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-04-05 13:17:28 -0500

    commit Tutorial

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,8 @@
+class ArticlesController < ApplicationController
+  def index
+    # The file app/views/articles/index.html.erb</p>
+    #          is rendered by default</p> 
+    #          because the method index is empty</p>
+    # in this controller app/controllers/articles_controller </p>
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get 'pages/hello'
+  get '/articles', to: 'articles#index'
+  # articles#index hace referencia al:
+  # metodo index (def index)
+  # en el controlador app/controllers/articles_controller.rb
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
